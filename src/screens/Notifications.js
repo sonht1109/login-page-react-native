a693f13c500945255359247c74a756f0169a53e4
sonht <sonht1109@gmail.com>
2021-02-09 16:28:44 +0700

    update

--- src/screens/Notifications.js
+++ src/screens/Notifications.js
@@ -205,7 +205,7 @@ const NotificationScreen = ({navigation}) => {
 
   return (
     <View style={styles.container}>
-      <StatusBar barStyle="dark-content"/>
+      {/* <StatusBar barStyle="dark-content"/> */}
       {/* <StatusBar backgroundColor="#FF6347" barStyle="light-content"/> */}
       <SwipeListView
         data={listData}
--- src/screens/Watch.js
+++ src/screens/Watch.js
@@ -1,11 +1,34 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, StyleSheet, Text, View } from 'react-native'
+import Onboarding from 'react-native-onboarding-swiper'
 
 export default function Watch() {
     return (
-        <View style={styles.container}>
-            <Text>Watch</Text>
-        </View>
+        // <View style={styles.container}>
+        //     <Text>Watch</Text>
+        // </View>
+        <Onboarding
+        pages={[
+            {
+                backgroundColor: "#a6e4d0",
+                image: <Image source={require('./img/onboarding-img1.png')} />,
+                title: "Connect to the world",
+                subtitle: "A new way to connect to the world"
+            },
+            {
+                backgroundColor: "#fdeb93",
+                image: <Image source={require('./img/onboarding-img2.png')} />,
+                title: "Share your favourites",
+                subtitle: "Share your thoughts with similar kind of people"
+            },
+            {
+                backgroundColor: "#e9bcbe",
+                image: <Image source={require('./img/onboarding-img3.png')} />,
+                title: "Become the star",
+                subtitle: "Let the spotlight capture you"
+            }
+        ]}
+        />
     )
 }
 
